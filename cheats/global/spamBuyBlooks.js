86f108a12e76c94033d19112f967afc501aab55c
laventurepublicservices-hash <laventurepublicservices@gmail.com>
2025-09-02 21:12:56 -0500

    Update spamBuyBlooks.js

--- cheats/global/spamBuyBlooks.js
+++ cheats/global/spamBuyBlooks.js
@@ -66,10 +66,5 @@
             decode += char;
             if (char == "/" && last == "*") break;
             last = char;
-        }
-        let iframe = document.querySelector("iframe");
-        const [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "(.+?)"/);
-        if (parseInt(time) <= 1693429947429 || iframe.contentWindow.confirm(error)) cheat();
-    }
-    img.onerror = img.onabort = () => (img.src = null, cheat());
+        
 })();
